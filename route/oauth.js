d27ab6bfea3345fa6aec8d1f2d3b1ae9386e7de3
Dan Habot <dan.habot@gmail.com>
2020-11-18 17:07:43 -0500

    fixes

--- route/oauth.js
+++ route/oauth.js
@@ -7,16 +7,7 @@ const bodyParser = require('body-parser')
 
 const route = express.Router()
 
-const APPROVED_APP_REDIRECTS = JSON.parse(process.env.APP_REDIRECTS)
-
 route.get('/:id', (req, res) => {
-  if (!APPROVED_APP_REDIRECTS.includes(req.query.redirect)) {
-    res.status(401).json({
-      'oauth_error': 'invalid redirect'
-    })
-    return
-  }
-
   Site.findById(req.params.id).exec((err, site) => {
     if (err || !site) {
       res.status(404).json({err: 'Site does not exist'})
@@ -26,12 +17,9 @@ route.get('/:id', (req, res) => {
       url.searchParams.append('client_id', site.clientID)
       url.searchParams.append('client_secret', site.clientSecret)
       url.searchParams.append('redirect_uri', `https://${process.env.DOMAIN}/auth/${req.params.id}/callback`)
-      url.searchParams.append('scope', ["identity", "create", "read", "update", "delete", "vote", "guildmaster"].join(','))
+      url.searchParams.append('scope', "identity,create,read,update,delete,vote,guildmaster")
       url.searchParams.append('permanent', 'true')
-      url.searchParams.append('state', JSON.stringify({
-        o: req.query.state,
-        r: req.query.redirect
-      }))
+      url.searchParams.append('state', req.query.state)
 
       res.redirect(url.toString())
     }
@@ -48,36 +36,14 @@ route.get('/:id/callback', (req, res) => {
         method: 'POST',
         body: `code=${req.query.code}&client_id=${site.clientID}&client_secret=${site.clientSecret}&grant_type=code`,
         headers: { 
-          'Content-Type': 'application/x-www-form-urlencoded'
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'X-AUTH-SERVER': 'SCROLL-FOR-RUQQUS'
         }
       })
 
-      var unpackState = JSON.parse(req.query.state)
-
-      f.then(rsp => rsp.text())
-        .then(text => {
-          let r = text
-          try {
-            r = {
-              ...JSON.parse(text),
-              state: unpackState.o
-            }
-          }
-          catch (e) {
-
-          }
-          finally {
-            // res.send(r)
-            // res.redirect(`${unpackState.r}?`)
-
-            res.writeHead(301, 'Redirect', {
-              Location: `${unpackState.r}?`
-            })
-            res.write(JSON.stringify(r))
-            res.end()
-          }
-        })
-
+      f.then(resp => {
+        resp.body.pipe(res)
+      })
       f.catch(e => res.status(500).json({err: e}))
     }
   })
@@ -93,27 +59,14 @@ route.post('/:id/refresh', bodyParser.json(), (req, res) => {
         method: 'POST',
         body: `refresh_token=${req.body.refresh_token}&client_id=${site.clientID}&client_secret=${site.clientSecret}&grant_type=refresh`,
         headers: { 
-          'Content-Type': 'application/x-www-form-urlencoded'
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'X-AUTH-SERVER': 'SCROLL-FOR-RUQQUS'
         }
       })
 
-      f.then(rsp => rsp.text())
-        .then(text => {
-          let r = text
-          try {
-            r = {
-              ...JSON.parse(text),
-              state: req.query.state
-            }
-          }
-          catch (e) {
-
-          }
-          finally {
-            res.send(r)
-          }
-        })
-
+      f.then(resp => {
+        resp.body.pipe(res)
+      })
       f.catch(e => res.status(500).json({err: e}))
     }
   })
