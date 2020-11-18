2eff4507cad69625636f76a446c906b600b7f91e
Dan Habot <dan.habot@gmail.com>
2020-11-18 17:00:05 -0500

    changes

--- route/sites.js
+++ route/sites.js
@@ -7,10 +7,12 @@ const requireLogin = require('../requireLogin')
 const route = express.Router()
 
 route.get('/', (req, res) => {
-  Site.find({}).select(['domain', 'name', 'clientID']).then(list => res.json(list))
+  console.log('Sending sites list')
+  Site.find({}).select(['domain', 'name']).then(list => res.json(list))
 })
 
 route.post('/new', requireLogin, bodyParser.urlencoded(), (req, res) => {
+  console.log('Editing document')
   Site.findOneAndUpdate(
     {owner: req.user._id}, 
     req.body, 
